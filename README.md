9e29a88206d8da4231cd3ef986d99f5ed2d87022
yokselzok <yokselzok@gmail.com>
2019-03-24 19:50:48 +0300

    Save

---
+++ README.md
@@ -0,0 +1,5 @@
+# SVG-фильтры
+
+Слайды доклада для [Я ❤ Frontend](https://events.yandex.ru/events/meetings/yalovefrontend/), 9 февраля 2019 года
+
+Движок: [Shower](https://github.com/shower/shower)
---
+++ assets/js/demo.js
@@ -0,0 +1,48 @@
+function Demo (config) {
+  const targetElem = document.querySelector(config.targetClass);
+  const {setExample, additionalProps} = config;
+
+  if(!targetElem) {
+    return;
+  }
+
+  const rangeContainer = targetElem.querySelector('.demo-range__container');
+  const rangeMin = targetElem.querySelector('.demo-range__min');
+  const rangeMax = targetElem.querySelector('.demo-range__max');
+  const example = targetElem.querySelector('.demo__example');
+
+  if(!example.innerHTML) {
+    example.innerHTML = config.fullName;
+  }
+
+  const rangeInput = document.createElement('input');
+  rangeInput.classList.add('demo-range__input');
+  rangeInput.type = 'range';
+  rangeInput.min = config.min;
+  rangeInput.max = config.max;
+  rangeInput.step = config.step;
+  rangeInput.value = rangeInput.max;
+
+  setVariation(rangeInput.value);
+
+  rangeMin.innerHTML = rangeInput.min;
+  rangeMax.innerHTML = rangeInput.max;
+  rangeContainer.appendChild(rangeInput);
+
+  rangeInput.addEventListener('input', function() {
+    setVariation(this.value);
+  });
+
+  function setVariation(val) {
+    let styleValues = [`"${config.name}" ${val}`];
+
+    if(additionalProps) {
+      additionalProps
+        .forEach(prop => {
+          styleValues.push(`"${prop.name}" ${prop.value}`);
+        });
+    }
+
+    example.style['font-variation-settings'] = styleValues.join(', ');
+  }
+}
